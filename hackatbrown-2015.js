15c46c3745ae15cd93e627da4daac89b5cb0200f
Amanda Liu <ajliu72@gmail.com>
2015-02-07 15:50:40 -0500

    Added auto-complete add place

--- hackatbrown-2015.js
+++ hackatbrown-2015.js
@@ -21,13 +21,41 @@ if (Meteor.isClient) {
     };
     
     map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    
+    var newPlace = document.getElementById("newPlace");
+    var autocomplete = new google.maps.places.Autocomplete(newPlace);
+    var autowindow = new google.maps.InfoWindow();
+    google.maps.event.addListener(autocomplete, 'place_changed', function(){
+      autowindow.close();
+      var place = autocomplete.getPlace();
+      if(!place.geometry){
+        return;
+      }
+      if(place.geometry.viewport){
+        map.fitBounds(place.geometry.viewport);
+      }else{
+        map.setCenter(place.geometry.location);
+        map.setZoom(17);
+      }
+      var address = '';
+      if (place.address_components) {
+        address = [
+          (place.address_components[0] && place.address_components[0].short_name || ''),
+          (place.address_components[1] && place.address_components[1].short_name || ''),
+          (place.address_components[2] && place.address_components[2].short_name || '')
+          ].join(' ');
+      }
+      autowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+    })
   }
 
   function createMarker(lat, lng){
     var marker = new google.maps.Marker({
-      position: {lat:lat, lng:lng},
-      map: map
-    })
+      position: new google.maps.LatLng(lat, lng),
+      
+    });
+    console.log(map);
+    marker.setMap(map);
   }
 
 
@@ -41,7 +69,12 @@ if (Meteor.isClient) {
   Template.create.events({
     "click #addMarker" : function(event){
       var lat = document.getElementById("lat").value;
+      lat = +lat;
+      console.log(lat);
       var lng = document.getElementById("lng").value;
+      lng = +lng;
+      console.log(lng);
+      createMarker(lat, lng);
     }
   })
 }
