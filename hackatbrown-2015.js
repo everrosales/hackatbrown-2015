5275eb73535d90702424216d0b5e5211941c4d0f
Amanda Liu <ajliu72@gmail.com>
2015-02-07 15:15:24 -0500

    Deleted counter button

--- hackatbrown-2015.js
+++ hackatbrown-2015.js
@@ -22,21 +22,6 @@ if (Meteor.isClient) {
     var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
   }
 
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
-
 
   Template.map.helpers({
     init : function() {
