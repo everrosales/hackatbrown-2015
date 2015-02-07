56a4545143e73460b57db80874faa9c42d255684
Amanda Liu <ajliu72@gmail.com>
2015-02-07 14:51:17 -0500

    Started basic map

--- hackatbrown-2015.js
+++ hackatbrown-2015.js
@@ -1,5 +1,26 @@
 if (Meteor.isClient) {
   // counter starts at 0
+
+  function initialize(){
+    
+    var mapOptions = {
+      center: new google.maps.LatLng(42.3581, -71.0636),
+      zoom: 8,
+      streetViewControl: false,
+      panControl: true,
+      panControlOptions: {
+        position: google.maps.ControlPosition.LEFT_CENTER
+      },
+      mapTypeControlOptions: {
+          style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+          position: google.maps.ControlPosition.BOTTOM_CENTER
+      },
+      zoomControlOptions: {
+          position: google.maps.ControlPosition.LEFT_CENTER
+      },
+    };
+    var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+  }
   Session.setDefault('counter', 0);
 
   Template.hello.helpers({
@@ -14,6 +35,14 @@ if (Meteor.isClient) {
       Session.set('counter', Session.get('counter') + 1);
     }
   });
+
+
+  Template.map.helpers({
+    init : function() {
+      google.maps.event.addDomListener(window, 'load', initialize);
+      return '';
+    }
+  })
 }
 
 if (Meteor.isServer) {
