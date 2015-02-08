68776ee65fe4ee6721b80bc8aec8445e7d292981
Amanda Liu <ajliu72@gmail.com>
2015-02-07 21:26:25 -0500

    Added current location functionality

--- hackatbrown-2015.js
+++ hackatbrown-2015.js
@@ -29,6 +29,50 @@ if (Meteor.isClient) {
     
     map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
     
+    if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = new google.maps.LatLng(position.coords.latitude,
+                                       position.coords.longitude);
+
+      var marker = new google.maps.Marker({
+        map: map,
+        position: pos,
+        title: "Current location"
+      });
+      infowindow = new google.maps.InfoWindow();
+      google.maps.event.addListener(marker, 'click', function(){
+        infowindow.setContent("Current location");
+        infowindow.open(map, this);
+      });
+      infowindow.setContent("Current location");
+      infowindow.open(map, marker);
+
+
+      map.setCenter(pos);
+    }, function() {
+      handleNoGeolocation(true);
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleNoGeolocation(false);
+  }
+}
+
+function handleNoGeolocation(errorFlag) {
+  if (errorFlag) {
+    var content = 'Error: The Geolocation service failed.';
+  } else {
+    var content = 'Error: Your browser doesn\'t support geolocation.';
+  }
+
+  var options = {
+    map: map,
+    position: new google.maps.LatLng(60, 105),
+    content: content
+  };
+
+  var infowindow = new google.maps.InfoWindow(options);
+  map.setCenter(options.position);
 
   }
 
